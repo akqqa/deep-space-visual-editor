75a44bcd78dbe47677932af1199e6aa9a2cb7521
Akqqa <alexk2002@gmail.com>
2026-08-20 15:19:39 +0100

    added welcome message

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,9 @@ import { initialiseEditor } from "./editor.js";
 import { initialisePersistence } from "./persistence.js";
 import { initialiseUI } from "./ui.js";
 
-if (!localStorage.getItem("multiSelect")) {
-  alert("New Feature! Multi Selection has been added. Check the CONTROLS for more info");
-  localStorage.setItem("multiSelect", "true");
+if (!localStorage.getItem("welcomeMessage")) {
+  alert("Welcome! This website is based off of the game The Message from Deep Space. You need a dictionary from that game to start! You can also join the discord to ask any questions: https://discord.com/invite/mMxWmvczCS");
+  localStorage.setItem("welcomeMessage", "true");
 }
 
 window.onload = () => {
