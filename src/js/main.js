2b036d27e7623c4737032d5055fb85733e62f3f6
Akqqa <alexk2002@gmail.com>
2026-08-20 15:29:50 +0100

    added steam page

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import { initialisePersistence } from "./persistence.js";
 import { initialiseUI } from "./ui.js";
 
 if (!localStorage.getItem("welcomeMessage")) {
-  alert("Welcome! This website is based off of the game The Message from Deep Space. You need a dictionary from that game to start! You can also join the discord to ask any questions: https://discord.com/invite/mMxWmvczCS");
+  alert("Welcome! This website is based off of the game The Message from Deep Space (https://store.steampowered.com/app/4080030). You need a dictionary from that game to start! You can also join the discord to ask any questions: https://discord.com/invite/mMxWmvczCS");
   localStorage.setItem("welcomeMessage", "true");
 }
 
